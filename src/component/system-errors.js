63a231550384d4cb2053aad5483465f55c7feed7
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-02-22 09:53:06 +0330

    add (main): system-errors

---
+++ src/component/system-errors.js
@@ -0,0 +1,24 @@
+// @ts-check
+
+class EmptySadadSettings extends Error {
+  /**@param {Error} error */
+  constructor(error) {
+    super(error.message);
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+class SadadSettingDeleted extends Error {
+  /**@param {Error} error */
+  constructor(error) {
+    super(error.message);
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+module.exports = {
+  EmptySadadSettings,
+  SadadSettingDeleted,
+};
