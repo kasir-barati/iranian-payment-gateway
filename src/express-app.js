9aa57d7cecf1830acad51775c147234e28e5ac6d
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-02-22 09:54:59 +0330

    add (main): express-app

---
+++ src/express-app.js
@@ -0,0 +1,39 @@
+// @ts-check
+const express = require('express');
+
+const app = express();
+
+const { appHost, appPort } = require('./config');
+
+const { sendResponse } = require('./endware/send-response');
+const {
+  expectedErrorHandler,
+} = require('./endware/expected-error-handler');
+const {
+  unexpectedErrorHandler,
+} = require('./endware/unexpected-error-handler');
+const {
+  endpointNotFoundErrorHandler,
+} = require('./endware/endpoint-not-found-error-handler');
+
+const paymentRouter = require('./router/payment');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(paymentRouter);
+app.use(endpointNotFoundErrorHandler);
+app.use(sendResponse);
+
+app.use(expectedErrorHandler);
+app.use(unexpectedErrorHandler);
+
+function listenExpressApp() {
+  app.listen(appPort, appHost, () => {
+    console.log(`Web API is up and running on ${appPort}`);
+  });
+}
+
+module.exports = {
+  app,
+  listenExpressApp,
+};
