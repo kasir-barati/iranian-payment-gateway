9fe3d65486211e00c047964847ade4e8b1e086a7
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-02-22 09:53:52 +0330

    add (main): validate middleware

---
+++ src/middleware/validate.js
@@ -0,0 +1,17 @@
+// @ts-check
+const { ValidatorError } = require('../util/error-response');
+
+/**@type {import('express').RequestHandler} */
+function validate(req, res, next) {
+  let errors = res.locals?.errors ?? null;
+
+  if (errors?.length > 0) {
+    return next(new ValidatorError(errors));
+  }
+
+  next();
+}
+
+module.exports = {
+  validate
+};
