a623c7b070068880170fbe517d155b80f310fbdc
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-02-27 10:28:09 +0330

    add (main): encrypt.js

---
+++ src/util/encrypt.js
@@ -0,0 +1,26 @@
+// @ts-check
+const { createHash, createCipheriv } = require('crypto');
+
+/**
+ *
+ * @param {string} data A utf8 string
+ * @param {string} key key would be hashed by md5 and shorten to maximum of 192 bits,
+ * @returns {string}
+ */
+function encryptTripleDes(data, key) {
+  let md5Key = createHash('md5')
+    .update(key)
+    .digest('hex')
+    // If you remove this substr, cipher throw Error: Invalid key length
+    //     this[kHandle].initiv(cipher, credential, iv, authTagLength);
+    .substr(0, 24);
+  let cipher = createCipheriv('des-ede3', md5Key, '');
+  let encrypted = cipher.update(data, 'utf8', 'base64');
+
+  encrypted += cipher.final('base64');
+  return encrypted;
+}
+
+module.exports = {
+  encryptTripleDes,
+};
